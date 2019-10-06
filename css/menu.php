8d06bfc34515493c464b36ae9bac27c6475e1dae
Maria Eduarda Galvao <mariaeduardagalvaodias8337@gmail.com>
2019-10-06 11:38:24 -0300

    Adicionando nova branch

---
+++ css/menu.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top" id="menu">
+            <!-- Brand -->
+
+            <img src="img/logo.png" alt="logo" style="width:40px;">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+				<li class="nav-item dropdown ">
+      				<a class="nav-link dropdown-toggle " href="#" id="navbardrop" data-toggle="dropdown">
+        				Ensino Médio
+      				</a>
+      				<div class="dropdown-menu">
+        				<a class="dropdown-item" href="1ano.php">1° Série</a>
+        				<a class="dropdown-item" href="2ano.php">2° Série</a>
+        				<a class="dropdown-item" href="3ano.php">3° Série</a>
+      				</div>
+    			</li>
+				<li class="nav-item dropdown" id="nomeusuario">
+      				<a class="nav-link dropdown-toggle " href="#" id="navbardrop" data-toggle="dropdown" >
+        				<?php
+						//$id = $_GET["id"];
+						//$selectnome = mysqli_query($conectar, "select nome_usuario from usuario where id_usuario='$id'");
+						//$resultado = mysqli_fetch_array($selectnome);
+						//echo "Olá, " .$resultado["nome_usuario"];
+						echo "Olá, " .$_SESSION['name_user'];
+					?>
+      				</a>
+      				<div class="dropdown-menu">
+						<a class="dropdown-item" href="perfil.php">Ver perfil</a>
+        				<a class="dropdown-item" href="sair.php">Sair</a>
+      				</div>
+    			</li>
+                </ul>
+            </div>
+        </nav>
